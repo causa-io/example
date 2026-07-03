14ec3b44661ee94a2311e1be85be4c2063d03aae
Flo <florent.vouin@gmail.com>
2026-07-03 09:25:23 +0200

    🎉 Configure the ordering service

---
+++ domains/ordering/service/jest.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('jest').Config} */
+const config = {
+  clearMocks: true,
+  coverageDirectory: '../coverage',
+  collectCoverageFrom: [
+    '**/*.{js,ts}',
+    '!**/index.ts',
+    '!**/*.spec.ts',
+    '!**/*.test.{ts,js}',
+    '!model/generated.ts',
+  ],
+  rootDir: 'src',
+  testEnvironment: 'node',
+  setupFiles: ['dotenv/config'],
+  setupFilesAfterEnv: ['jest-extended/all'],
+  testMatch: ['**/*.spec.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleFileExtensions: ['js', 'ts'],
+  transform: { '^.+\\.(t|j)s?$': ['@swc/jest', { sourceMaps: 'inline' }] },
+  moduleNameMapper: { '^(\\.{1,2}/.*)\\.js$': '$1' },
+};
+
+export default config;
